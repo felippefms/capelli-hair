7ab83debd5db744941320620cb23a9d29fe62392
Felipe Madureira de Souza <Felippefms3@gmail.com>
2024-01-04 23:03:59 -0300

    [update]

---
+++ app/api/securityRoutes.js
@@ -0,0 +1,8 @@
+export const APP_ROUTES = {
+    public: {
+        
+    },
+    private: {
+        name: '/admin-page'
+    }
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+'use client'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import { usePathname } from 'next/navigation'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -7,15 +9,30 @@ const montserrat = Montserrat({
   weight: '400'
 })
 
-export const metadata = {
-  title: 'Capelli Hair',
-  description: 'Capelli Hair',
-}
+// export const metadata = {
+//   title: 'Capelli Hair',
+//   description: 'Capelli Hair',
+// }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-br">
-      <body className={montserrat.className}>{children}</body>
-    </html>
-  )
+  const route = usePathname();
+  console.log(route);
+  
+  if (true) {
+    return (
+      <html lang="pt-br">
+        <body className={montserrat.className}>
+          {isPublicPage && children}
+        </body>
+      </html>
+    )
+  } else {
+    return (
+      <html lang="pt-br">
+        <body className={montserrat.className}>
+          {!isPublicPage && <PrivateRoute>{children}</PrivateRoute>}
+        </body>
+      </html>
+    )
+  }
 }

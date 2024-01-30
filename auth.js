b3d89605c678f69308664cfad462e57d224a67dd
Felipe Madureira de Souza <Felippefms3@gmail.com>
2024-01-30 19:09:20 -0300

    update

--- auth.js
+++ auth.js
@@ -2,13 +2,10 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import Facebook from "next-auth/providers/facebook"
 
-import SpringbootAdapter from './adapters/springboot'
-
 export const {
   handlers: { GET, POST },
   auth, useSession,
 } = NextAuth({
-  // adapter: SpringbootAdapter,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 import { auth } from "./auth"
 export default auth((req) => {
-
+  const isLoggedIn = !!req.auth;
+  console.log("Route:", req.nextUrl.pathname);
+  console.log("IS LOGGED: ", isLoggedIn);
 })
 
 export const config = {

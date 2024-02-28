a2eb9a8683cba9398359ddf93ef0c3bf67016d49
Felipe Madureira de Souza <Felippefms3@gmail.com>
2024-02-27 21:12:09 -0300

    Update ChartStage.jsx

--- components/chartStages/ChartStage.jsx
+++ components/chartStages/ChartStage.jsx
@@ -52,7 +52,7 @@ export default function ChartStage(props) {
 
                     <div className='w-full flex flex-col items-center'>
                         <div>
-                            <button className='w-[288px] h-[53px] mt-10 rounded-[10px] text-[#fff] bg-[#9D8168]' onClick={() => setChartStage('revision')}>Ir para o pagamento</button>
+                            <button className='w-[288px] h-[53px] mt-10 rounded-[10px] text-[#fff] bg-[#9D8168]' onClick={() => setChartStage('identification')}>Ir para o pagamento</button>
                         </div>
                         <div>
                             <button className='w-[288px] h-[53px] my-6 rounded-[10px] border border-[#9D8168] text-[#9D8168]'>Continuar comprando</button>
